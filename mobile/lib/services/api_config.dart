d12018840faec02df52c1b6b563e179161cb8c10
FionnLaesser <flaesser@bluewin.ch>
2026-06-22 09:37:35 +0200

    chore: update branding and backend address

--- mobile/lib/services/api_config.dart
+++ mobile/lib/services/api_config.dart
@@ -1,6 +1,6 @@
 class ApiConfig {
   // Android Emulator: 10.0.2.2 = localhost des Host-Rechners
   // Echtes Gerät im selben WLAN: lokale IP des Host-Rechners (z.B. 192.168.1.x)
-  static const String baseUrl = 'http://10.184.176.145:8000';
+  static const String baseUrl = 'http://10.222.166.145:8000';
   static const Duration requestTimeout = Duration(seconds: 8);
 }
